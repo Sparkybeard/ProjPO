00287ba2c4a7d65b2bd65883d67068fe544b3bf4
Sparkybeard <34483149+Sparkybeard@users.noreply.github.com>
2018-11-12 12:58:32 +0000

    Add files via upload

---
+++ sth/app/main/DoSave.java
@@ -0,0 +1,45 @@
+package sth.app.main;
+
+import java.io.IOException;
+
+import pt.tecnico.po.ui.Command;
+import pt.tecnico.po.ui.Input;
+import sth.core.SchoolManager;
+import sth.core.exception.ImportFileException;
+import sth.app.main.Label;
+
+//FIXME import other classes if needed
+
+/**
+ * 4.1.1. Save to file under current name (if unnamed, query for name).
+ */
+public class DoSave extends Command<SchoolManager> {
+  protected Input<String> _filename;
+
+  //FIXME add input fields if needed
+
+  /**
+   * @param receiver
+   */
+  public DoSave(SchoolManager receiver) {
+    super(Label.SAVE, receiver);
+    _filename = _form.addStringInput("Introduza o nome do ficheiro: ");
+    //FIXME initialize input fields if needed
+  }
+
+  /** @see pt.tecnico.po.ui.Command#execute() */
+  @Override
+  public final void execute() {
+      //FIXME implement command
+      _form.parse();
+      try {
+          _receiver.saveSerializable(_filename);
+          _display.addLine("Ficheiro guardado: ");
+
+      } catch (ImportFileException e) {
+          _display.addLine("Impossível importar ficheiro.");
+      }
+
+  }
+
+}
---
+++ sth/core/School.java
@@ -0,0 +1,35 @@
+package sth.core;
+
+//FIXME import other classes if needed
+
+import sth.core.exception.BadEntryException;
+import sth.core.exception.NoSuchPersonIdException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * School implementation.
+ */
+public class School implements java.io.Serializable {
+
+  /** Serial number for serialization. */
+  private static final long serialVersionUID = 201810051538L;
+
+  //FIXME define object fields (attributes and, possibly, associations)
+  //FIXME implement constructors if needed
+
+  /**
+   * @param filename
+   * @throws BadEntryException
+   * @throws IOException
+   */
+  void importFile(String filename) throws IOException, BadEntryException {
+    //FIXME implement text file reader
+    File file;
+
+  }
+  
+  //FIXME implement other methods
+
+}
